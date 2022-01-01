09cf4d7b8adde0222b5eb5688da97d2c146152f2
m.fathi.farhat210503@gmail.com <m.fathi.farhat210503@gmail.com>
2022-01-01 13:58:04 +0700

    first commit

---
+++ migrations/20211221110144-create-products-tabel.js
@@ -0,0 +1,37 @@
+// "use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("products", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      brand: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      createAT: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      updateAT: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("products");
+  },
+};
---
+++ models/Product.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      tableName: "products",
+    }
+  );
+  return Product;
+};
